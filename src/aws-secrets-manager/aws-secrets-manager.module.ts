6272dd6ca4041f1521cf0204cf64f1552da40de4
razzkumar <razzkumar.dev@gmail.com>
2022-08-17 13:16:14 +0545

    Add aws secrect module

---
+++ src/aws-secrets-manager/aws-secrets-manager.module.ts
@@ -0,0 +1,41 @@
+import { DynamicModule, Global, Module } from '@nestjs/common';
+
+import { AWS_SECRETS_ARN, AWS_SECRETS_MANAGER_TOKEN, AWS_SECRETS_SET_ENV } from './contstants';
+import { SecretsManager } from '@aws-sdk/client-secrets-manager';
+import { AWSSecretsService } from './aws-secrets-manager.service';
+
+export interface AWSSecretsManagerModuleOptions {
+  secretsManager: SecretsManager;
+  isSetToEnv: boolean;
+  secretsArn: string[];
+}
+
+@Global()
+@Module({})
+export class AWSSecretsManagerModule {
+  static forRoot({
+    secretsManager,
+    isSetToEnv = true,
+    secretsArn,
+  }: AWSSecretsManagerModuleOptions): DynamicModule {
+    return {
+      module: AWSSecretsManagerModule,
+      providers: [
+        AWSSecretsService,
+        {
+          provide: AWS_SECRETS_MANAGER_TOKEN,
+          useValue: secretsManager,
+        },
+        {
+          provide: AWS_SECRETS_SET_ENV,
+          useValue: isSetToEnv,
+        },
+        {
+          provide: AWS_SECRETS_ARN,
+          useValue: secretsArn,
+        }
+      ],
+      exports: [AWSSecretsService],
+    };
+  }
+}
---
+++ src/aws-secrets-manager/index.ts
@@ -0,0 +1,1 @@
+export * from "./aws-secrets-manager.module";
