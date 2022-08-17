1d0cdb727c0c9e918690c7382d4e7c8a6403cf9c
razzkumar <razzkumar.dev@gmail.com>
2022-08-17 19:30:49 +0545

    Add pre-hook scripts

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AWSSecretsManagerModule } from '../src/aws-secrets-manager';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AWSSecretsManagerModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
