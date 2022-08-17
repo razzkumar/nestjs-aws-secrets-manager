ebc05ff257bf514f9d25bea91f29ebe1a194fb21
razzkumar <razzkumar.dev@gmail.com>
2022-08-17 18:15:48 +0545

    add quick-start-secrets-to-env sample

---
+++ samples/quick-start-secrets-to-env/src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { SecretsManagerClient } from '@aws-sdk/client-secrets-manager';
+import { AWSSecretsManagerModule, AWSSecretsManagerModuleOptions, } from 'nestjs-aws-secrets-manager';
+
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
+
+const AWSSecretsManagerProps: AWSSecretsManagerModuleOptions = {
+  secretsManager: new SecretsManagerClient({
+    region: "ap-south-1"
+  }),
+  isSetToEnv: true,
+  secretsSource: "test/sm" // OR array or secrets name or ARN  [ "db/prod/config" ,"app/prod/config"],
+};
+
+
+@Module({
+  imports: [
+    AWSSecretsManagerModule.forRoot(AWSSecretsManagerProps)
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
