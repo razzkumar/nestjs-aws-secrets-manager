163697dca2f4d6e773a02941088f7723977641eb
razzkumar <razzkumar.dev@gmail.com>
2022-08-17 14:15:37 +0545

    Add debug logging options

--- src/aws-secrets-manager/aws-secrets-manager.module.ts
+++ src/aws-secrets-manager/aws-secrets-manager.module.ts
@@ -1,13 +1,14 @@
 import { DynamicModule, Global, Module } from '@nestjs/common';
-
-import { AWS_SECRETS_ARN, AWS_SECRETS_MANAGER_TOKEN, AWS_SECRETS_SET_ENV } from './contstants';
 import { SecretsManagerClient } from '@aws-sdk/client-secrets-manager';
+
 import { AWSSecretsService } from './aws-secrets-manager.service';
+import { AWS_SECRETS_ARN, AWS_SECRETS_DEBUG, AWS_SECRETS_MANAGER_TOKEN, AWS_SECRETS_SET_ENV } from './contstants';
 
 export interface AWSSecretsManagerModuleOptions {
   secretsManager: SecretsManagerClient;
-  isSetToEnv: boolean;
+  isSetToEnv?: boolean;
   secretsArn: string[];
+  isDebug?: boolean;
 }
 
 @Global()
@@ -17,6 +18,7 @@ export class AWSSecretsManagerModule {
     secretsManager,
     isSetToEnv = true,
     secretsArn,
+    isDebug = false,
   }: AWSSecretsManagerModuleOptions): DynamicModule {
     return {
       module: AWSSecretsManagerModule,
@@ -33,6 +35,10 @@ export class AWSSecretsManagerModule {
         {
           provide: AWS_SECRETS_ARN,
           useValue: secretsArn,
+        },
+        {
+          provide: AWS_SECRETS_DEBUG,
+          useValue: isDebug
         }
       ],
       exports: [AWSSecretsService],
